3484920184d9e343849d8b62be6326e02e68cf39
Jeffrey Zheng <jeffreyzheng360@gmail.com>
2022-06-27 22:57:09 -0500

    Add ProcessingArrays.java

--- Chapter07-one-dimensional-arrays/Intro.java
+++ Chapter07-one-dimensional-arrays/Intro.java
@@ -9,41 +9,6 @@ public class Intro
 {
     public static void main(String[] args)
     {
-        double grade1 = 95;
-        double grade2 = 80;
-        double grade3 = 98;
-        double grade4 = 84;
-        double grade5 = 88;
-        double grade6 = 87;
-
-        double average = (grade1 + grade2 + grade3 + grade4 + grade5 + grade6)/6;
-        int count = 0;
-        if (grade1 > average)
-        {
-            count++;
-        }
-        if (grade2 > average)
-        {
-            count++;
-        }
-        if (grade3 > average)
-        {
-            count++;
-        }
-        if (grade4 > average)
-        {
-            count++;
-        }
-        if (grade5 > average)
-        {
-            count++;
-        }
-        if (grade6 > average)
-        {
-            count++;
-        }
-        System.out.printf(("The average grade is %5.2f\n"), average);
-
-        System.out.printf("the number of grades that are above the average is: %d\n", count);
+        
     }
 }
---
+++ Chapter07-one-dimensional-arrays/ProcessingArrays.java
@@ -0,0 +1,26 @@
+public class ProcessingArrays {
+    public static void main(String[] args){
+        // 1. Initiazing arrays with input values
+
+
+        // 2. Initializing arrays with random values
+
+
+        // 3. Printing arrays
+
+
+        // 4. Summing all elements
+
+
+        // 5. Finding the largest element
+
+
+        // 6. Finding the smallest index of the largest element
+
+
+        // 7. Random shuffling
+
+
+        // 8. Foreach loops
+    }
+}
